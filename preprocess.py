e35c560e9d337168da45ef1d55911934ef4c6bb1
Aditya <adityasinghak17@gmail.com>
2025-10-28 20:32:26 +0530

    chore: update project structure and imports

--- preprocess.py
+++ preprocess.py
@@ -3,3 +3,12 @@ from llm_helper import llm
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.exceptions import OutputParserException
+
+def process_posts(raw_file_path, processed_file_path=None):
+    with open(raw_file_path, encoding='utf-8') as file:
+        posts = json.load(file)
+        enriched_posts = []
+        for post in posts:
+            metadata = extract_metadata(post['text'])
+            post_with_metadata = post | metadata
+            enriched_posts.append(post_with_metadata)
