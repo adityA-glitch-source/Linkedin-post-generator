018a82bf3c70af4dfede39da4ec210b4484d4b1e
Aditya <adityasinghak17@gmail.com>
2025-10-28 20:36:19 +0530

    chore: code maintenance and organization

--- preprocess.py
+++ preprocess.py
@@ -44,3 +44,40 @@ def extract_metadata(post):
     except OutputParserException:
         raise OutputParserException("Context too big. Unable to parse jobs.")
     return res
+
+
+def get_unified_tags(posts_with_metadata):
+    unique_tags = set()
+    # Loop through each post and extract the tags
+    for post in posts_with_metadata:
+        unique_tags.update(post['tags'])  # Add the tags to the set
+
+    unique_tags_list = ','.join(unique_tags)
+
+    template = '''I will give you a list of tags. You need to unify tags with the following requirements,
+    1. Tags are unified and merged to create a shorter list. 
+       Example 1: "Jobseekers", "Job Hunting" can be all merged into a single tag "Job Search". 
+       Example 2: "Motivation", "Inspiration", "Drive" can be mapped to "Motivation"
+       Example 3: "Personal Growth", "Personal Development", "Self Improvement" can be mapped to "Self Improvement"
+       Example 4: "Scam Alert", "Job Scam" etc. can be mapped to "Scams"
+    2. Each tag should be follow title case convention. example: "Motivation", "Job Search"
+    3. Output should be a JSON object, No preamble
+    3. Output should have mapping of original tag and the unified tag. 
+       For example: {{"Jobseekers": "Job Search",  "Job Hunting": "Job Search", "Motivation": "Motivation}}
+
+    Here is the list of tags: 
+    {tags}
+    '''
+    pt = PromptTemplate.from_template(template)
+    chain = pt | llm
+    response = chain.invoke(input={"tags": str(unique_tags_list)})
+    try:
+        json_parser = JsonOutputParser()
+        res = json_parser.parse(response.content)
+    except OutputParserException:
+        raise OutputParserException("Context too big. Unable to parse jobs.")
+    return res
+
+
+if __name__ == "__main__":
+    process_posts("data/raw_posts.json", "data/processed_posts.json")
