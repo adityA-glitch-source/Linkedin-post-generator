0743141e7c73da54aeb42977458ff3b510eb2c43
Aditya <adityasinghak17@gmail.com>
2025-10-28 20:33:13 +0530

    chore: update project structure and imports

--- preprocess.py
+++ preprocess.py
@@ -12,3 +12,11 @@ def process_posts(raw_file_path, processed_file_path=None):
             metadata = extract_metadata(post['text'])
             post_with_metadata = post | metadata
             enriched_posts.append(post_with_metadata)
+    unified_tags = get_unified_tags(enriched_posts)
+    for post in enriched_posts:
+        current_tags = post['tags']
+        new_tags = {unified_tags[tag] for tag in current_tags}
+        post['tags'] = list(new_tags)
+
+    with open(processed_file_path, encoding='utf-8', mode="w") as outfile:
+        json.dump(enriched_posts, outfile, indent=4)
